149636fb44d75323700f72fad1ac9645898cf469
Lucas Tavares <l.tavaresc@hotmail.com>
2024-04-05 20:50:45 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,167 @@
-import Image from "next/image";
+"use client";
+"use client";
+import { useState, ChangeEvent } from "react";
+import { saveAs } from "file-saver";
+import { Document, Packer, Paragraph, TextRun, ImageRun } from "docx";
 
 export default function Home() {
+  const [title, setTitle] = useState<string>("");
+  const [finalText, setFinalText] = useState<string>("");
+  const [selectedImage, setSelectedImage] = useState<string>("");
+  const [newImageName, setNewImageName] = useState<string>("");
+
+  const handleSelectedImage = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.result && typeof reader.result === "string") {
+          setSelectedImage(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+      setNewImageName(file.name);
+    }
+  };
+
+  const generateReport = () => {
+    if (!selectedImage) return;
+
+    const doc = new Document({
+      sections: [
+        {
+          properties: {},
+          children: [
+            new Paragraph({ 
+              alignment: "center",
+              children: [
+                new TextRun({
+                  text: title,
+                  bold: true,
+                  size: 60,
+                }),
+              ],
+            }),
+            new Paragraph({ 
+              alignment: "center",
+              children: [
+                new TextRun({
+                  text: "",
+                  break: 6,
+                }),
+              ],
+            }),
+            new Paragraph({
+              alignment: "center",
+              children: [
+                new ImageRun({
+                  data: selectedImage,
+                  transformation: {
+                    width: 450,
+                    height: 400,
+                  },
+                  altText: {
+                    title: newImageName,
+                    description: newImageName,
+                    name: newImageName,
+                  },
+                }),
+                new TextRun({
+                  text: `CAPTION: ${newImageName}`,
+                  break: 2,
+                }),
+              ],
+            }),
+            new Paragraph({
+              children: [
+                new TextRun({
+                  break: 3,
+                  text: finalText,
+                }),
+              ],
+            }),
+          ],
+        },
+      ],
+    });
+
+    Packer.toBlob(doc).then((blob) => {
+      saveAs(blob, "report.docx");
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='max-w-md mx-auto bg-white rounded-lg overflow-hidden shadow-md p-4'>
+        <h2 className='text-2xl font-semibold mb-2'>Report Generator</h2>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <label htmlFor='title' className='block text-gray-700 font-bold mb-2'>
+          Título do relatório
+        </label>
+        <input
+          type='text'
+          id='title'
+          name='title'
+          placeholder='Escreva o título do relatório'
+          className='w-full bg-gray-200 border border-gray-300 rounded-md py-2 px-4 mb-4 focus:outline-none focus:bg-white focus:border-gray-500 text-black'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
+
+        <label htmlFor='image' className='block text-gray-700 font-bold mb-2'>
+          Adiconar imagem
+        </label>
+        <input
+          type='file'
+          id='image'
+          name='image'
+          accept='image/*'
+          className='w-full bg-gray-200 border border-gray-300 rounded-md py-2 px-4 mb-4 focus:outline-none focus:bg-white focus:border-gray-500 text-black'
+          onChange={handleSelectedImage}
+        />
+
+        {selectedImage && (
+          <div className='mb-4'>
+            <label
+              htmlFor='newImageName'
+              className='block text-gray-700 font-bold mb-2'
+            >
+              Novo nome do arquivo de imagem
+            </label>
+            <input
+              type='text'
+              id='newImageName'
+              name='newImageName'
+              className='w-full bg-gray-200 border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:bg-white focus:border-gray-500 text-black'
+              value={newImageName}
+              onChange={(e) => setNewImageName(e.target.value)}
+            />
+          </div>
+        )}
+
+        <label
+          htmlFor='final-text'
+          className='block text-gray-700 font-bold mb-2'
+        >
+          Texto final
+        </label>
+        <textarea
+          id='final-text'
+          name='final-text'
+          rows={4}
+          className='w-full bg-gray-200 border border-gray-300 rounded-md py-2 px-4 mb-4 focus:outline-none focus:bg-white focus:border-gray-500 text-black'
+          placeholder='Adione o texto final do relatório'
+          value={finalText}
+          onChange={(e) => setFinalText(e.target.value)}
+        ></textarea>
+
+        <button
+          onClick={generateReport}
+          className='w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        >
+          Gerar Relatório
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
